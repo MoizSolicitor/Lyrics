077907ddb6c71156b13fea28ab631183354e7b40
Moiz8686 <moizlink08@gmail.com>
2022-10-12 18:41:21 +0500

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Error = () => (
+  <div className='w-full justify-items items-center'>
+    <h1 className='font-bold text-2xl text-white'>Something went wrong,please try again.</h1>
+
+  </div>
+);
+
+export default Error;
